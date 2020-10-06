fb4b960bd9a374eb4f89d36059dd58a58cf5b460
brooke <brooke.meleah@gmail.com>
2020-10-06 19:22:53 +1300

    tidy up index

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const config = require('./config.json');
 
 var lindwormDeck = require('./deck.js')
 
-lindwormDeck.load()
-lindwormDeck.draw()
-lindwormDeck.handStatus()
-lindwormDeck.save()
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
